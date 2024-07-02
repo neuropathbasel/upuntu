0dce5eca3894bdddf767f2a3f7686be98f129da7
Neuropathology Lab at Institute for Medical Genetics and Pathology, University Hospital Basel, Switzerland <juergen.hench@usb.ch>
2024-07-02 13:20:41 +0200

    Update manual_update.sh
    added the removal of unattended upgrades which negatively impacts on the GPU when updating the driver while processes are running on the GPU.

--- manual_update.sh
+++ manual_update.sh
@@ -6,5 +6,6 @@ sudo apt -y update
 sudo apt upgrade
 sudo apt -y autoremove
 sudo apt -y autoclean
+sudo apt -y remove unattended-upgrades
 sudo chmod a-x /usr/bin/update-notifier
 sudo chmod a-x /usr/bin/update-manager
